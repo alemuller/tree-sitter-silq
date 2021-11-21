e4684c9be79d0bde71c6317cb8825fab63eca79e
Alexandre Muller <muller@outlook.com>
2021-11-21 18:06:36 -0300

    update highlight query

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,18 +1,90 @@
+;; Keywords
 [
- ":="
+  "as"
+  "coerce"
+] @keyword
+
+[
+  "if"
+  "else"
+] @conditional
+
+[
+  "def"
+  "return"
+] @keyword.function
+
+[
+  "import"
+] @include
+
+;; Punctuations
+[
+  ":="
+  "^" "×" "→" ; type ops
 ] @operator
 
 [
- ";"
+  ";" "," ":" "."
 ] @punctuation.delimiter
 
-(const_Z) @number
+[
+  "(" ")"
+  "[" "]"
+  "{" "}"
+] @punctuation.bracket
 
 [
-  (const_Q)
-  (const_pi)
-] @float
+  "!"
+  "mfree"
+  "qfree"
+  "const"
+] @annotation
 
-(const_B) @boolean
+;; Literals
+
+(string)    @string
+(character) @character
+(const_Z)   @number
+(const_Q)   @float
+(const_B)   @boolean
+
+[
+  "δ" "∞"
+] @constant.macro
+
+;; Functions
+[
+  "⌈" "⌉" ; ceil
+  "⌊" "⌋" ; floor
+] @function
+
+[
+  "δ" "λ"
+] @function
+
+;; Names
+(function_declr
+  name: (identifier) @function)
+
+(function_def
+  name: (identifier) @function)
+
+;; Built-ins
+[
+  "𝟙"
+  "𝔹"
+  "ℕ"
+  "ℤ"
+  "ℚ"
+  "ℝ"
+  "ℂ"
+  "int"
+  "uint"
+] @type.builtin
 
 (comment) @comment
+
+;; LINT
+(tuple_type
+  "const" @error)
